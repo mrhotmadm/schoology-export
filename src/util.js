997f205872c1d831c9d0b7e936be460bb022dbd2
azharr <mrblackcto@outlook.com>
2025-06-13 22:53:45 -0700

    implement ratelimit check #4

--- src/util.js
+++ src/util.js
@@ -1,18 +1,44 @@
+let previousRequestTime = null;
+let requestCount = 0;
+
+export const checkRateLimit = async () => {
+    if (previousRequestTime) {
+        const timeSinceLastRequest = Date.now() - previousRequestTime;
+        
+        if (timeSinceLastRequest > 5000) {
+            // Reset if last request was more than 5 seconds ago
+            previousRequestTime = null;
+            requestCount = 0;
+        } else if (requestCount >= 10) { // max 10 requests for precaution
+            console.warn(`[schoology-export] Cautionary rate limit exceeded. Waiting for 5 seconds...`);
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            requestCount = 0;
+        };
+        console.log({timeSinceLastRequest, requestCount});
+    };
+};
+
 export const fetchPageHTML = async (url, callback = () => {}, delayMs = 250) => {
     await new Promise(resolve => setTimeout(resolve, delayMs));
 
-    const response = await fetch(url, {
-        method: 'GET',
-        // credentials: 'include'
-    });
+    // Rate limiting to avoid overwhelming the server (prediction: 15 requests per 5 seconds)
+    await checkRateLimit();
 
-    if (!response.ok) {
-        console.error(`Failed to fetch ${url}: ${response.statusText}`);
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            // credentials: 'include'
+        }).finally(() => {
+            requestCount++;
+            previousRequestTime = Date.now();
+        });
+
+        const text = await response.text();
+        return callback(text);
+    } catch (error) {
+        console.error(`[schoology-export] Error fetching page HTML from ${url}:`, error);
         return callback(null);
     };
-
-    const text = await response.text();
-    return callback(text);
 };
 
 export const extractElement = (html, selector) => {
